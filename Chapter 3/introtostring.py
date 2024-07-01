cd9dba5fea4b67e19b8a7ac3d6eaa59fe5851413
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-01 10:55:45 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 3

---
+++ Chapter 3/introtostring.py
@@ -0,0 +1,11 @@
+name ="shivam"
+b = 'me'
+c = '1'
+nameshort = name[0:3]
+
+print(name[0:6])
+print(name[-1:-2])
+print(nameshort)
+print(len(name))
+print(name.endswith("a"))
+print(name.startswith("a"))
