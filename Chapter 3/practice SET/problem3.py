4cfd5cb0fb7e032778474ada0d57737cfed65b6a
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-02 18:50:22 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 3

---
+++ Chapter 3/practice SET/problem3.py
@@ -0,0 +1,9 @@
+name = "Shivam is here  to help  you"
+
+print(name.find("  ",  "")) 
+
+#strings are Immutable
+
+# name = "Shivam is here  to help  you"
+
+# print(name.replace("  ",  "")) 
---
+++ Chapter 3/practice SET/problem4.py
@@ -0,0 +1,5 @@
+#write the program to format the letter using escape sequence characters
+
+letter = "Dear Shivam,\n\t this Vs Code is.\n Now Updated"
+
+print(letter)
