22f0d7b95a4e1ac839623add1edfcf51d91fb01f
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-08 23:05:18 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 5 - sets_dictionary

---
+++ Chapter 5 - sets_dictionary/01_dict.py
@@ -0,0 +1,13 @@
+marks ={
+    "shivam" : 100,
+    "Don" : 65,
+    "Rohan" : 48
+}
+
+print(marks,type(marks))
+
+print("Marks of shivam is:")
+print(marks["shivam"])
+
+# output = {'shivam': 100, 'Don': 65, 'Rohan': 48} <class 'dict'>
+
---
+++ Chapter 5 - sets_dictionary/01_dictMethods.py
@@ -0,0 +1,8 @@
+marks ={
+    "Shivam" : 100,
+    "Alex" : 99,
+    "dharma":98,
+    0: "Harry"
+}
+
+print(marks.keys())
