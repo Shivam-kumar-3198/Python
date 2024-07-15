8c3d91170ae462c2efd5b22140cbd968b8cbae47
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-15 19:10:16 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 6

---
+++ Chapter 6/age.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Your age :"))
+
+if(age >=18):
+    print("yes you are ok")
+
+else:
+    print("Go back to home")
---
+++ Chapter 6/multicondition.py
@@ -0,0 +1,17 @@
+a = int(input("Enter Your Age:"))
+
+if(a>=18):
+    print("You are above the age of concent")
+    print("Good for You")
+
+elif(a<0):
+    print("You are entering an invalid negative age")
+
+elif(a==0):
+    print("You are entering 0 which is not valid age")
+
+else:
+    print("You are below the age of concent")
+
+print("End of Program")        
+
