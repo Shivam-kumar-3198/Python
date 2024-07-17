5ba21d62368185ecc8d7abfc2e11badc5b6f5101
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-17 19:07:52 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 6\chapter6_Practice set

---
+++ Chapter 6/chapter6_Practice set/prob1.py
@@ -0,0 +1,16 @@
+a0 = int(input("Enter number 1 :"))
+a1 = int(input("Enter number 2 :"))
+a2 = int(input("Enter number 3 :"))
+a3 = int(input("Enter number 4 :"))
+
+if(a0>a1 and a0>a2 and a0>a3):
+    print("Greatest Number is :" ,a0)
+
+if(a1>a0 and a1>a2 and a1>a3):
+    print("Greatest Number is :" , a1)
+
+if(a2>a0 and a2>a1 and a2>a3):
+    print("Greatest Number is :" , a2)
+
+if(a3>a0 and a3>a1 and a3>a2):
+    print("Greatest Number is :" , a3)        
