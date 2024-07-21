acf54dbb81a0024e0bab26e9d38fdf5d0bc6a421
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-21 19:07:26 +0530

    c:\Users\thisi\OneDrive\Pictures\Desktop\python\Python\Chapter 6\chapter6_Practice set\prob4.py

---
+++ Chapter 6/chapter6_Practice set/prob4.py
@@ -0,0 +1,8 @@
+# write a program to print a username contains less than 10 letters or not
+username = input("Enter username")
+
+if(len(username)<10):
+    print("Your Username contains 10 characters ")
+
+else:
+    print("Your Username contains more than 10 characters ")
