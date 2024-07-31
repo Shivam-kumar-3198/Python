61799998dfc3b265d952f319dee4fe51195095a1
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-31 23:03:48 +0530

    .

--- Chapter 6/chapter6_Practice set/prob3.py
+++ Chapter 6/chapter6_Practice set/prob3.py
@@ -6,7 +6,7 @@ p4 = "Click this"
 message = input("Enter your Comment: ")
 
 if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
-    print("This comment is a spam")
+    print("This comment is a spam be aware from that")
 
 else:
     print("This comment is not a Spam")    
