0016e96367a1a49c0cdc16679c294a50cc402bbe
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-13 18:22:04 +0530

    .

---
+++ chapter 7/07_break_continue.py
@@ -0,0 +1,11 @@
+for i in range(100):
+    if(i == 34):
+        break
+    print(i) 
+    # break stops the loop
+
+for i in range(100):
+    if(i == 34):
+        continue
+    print(i)
+    # continue only leave the targeted value
