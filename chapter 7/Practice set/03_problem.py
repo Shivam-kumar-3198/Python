f96dd697c41f1150a3af97d8287c7b0087bde655
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-16 17:53:11 +0530

    .

--- chapter 7/Practice set/03_problem.py
+++ chapter 7/Practice set/03_problem.py
@@ -1,1 +1,4 @@
-n = input("Enter")
+n = int(input("Enter the table you want to print : "))
+
+for i in range(1,11):
+    print(f"{n} x {i} ={n * i}")
---
+++ chapter 7/Practice set/04_problem2.py
@@ -0,0 +1,5 @@
+name = ["Shivam", "Sohaan", "Sachin","Rahul"]
+
+for letter in name:
+    if(letter.startswith("S")):
+        print(f"Hello{letter}")
