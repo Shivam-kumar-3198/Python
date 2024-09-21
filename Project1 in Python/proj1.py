12ff72b13d01b337a414cfdcc61f80897ae00b4e
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-21 23:10:12 +0530

    .

--- Project1 in Python/proj1.py
+++ Project1 in Python/proj1.py
@@ -1,2 +1,3 @@
-# # We all have played snake, water gun game in our childhood.If you haven't google the rules and
-# write the python code capable of playing by user 
+# # # We all have played snake, water gun game in our childhood.If you haven't google the rules and
+# # write the python code capable of playing by user 
+# write the code for it 
