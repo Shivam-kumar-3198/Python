16d01c9c29f9d5c69e11964aef6fdc20670e0f3a
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-27 23:21:04 +0530

    .

--- Project1 in Python/proj1.py
+++ Project1 in Python/proj1.py
@@ -2,7 +2,7 @@
 # # write the python code capable of playing by user 
 # write the code for it 
 computer = -1
-you = input("Enter Your Choice : ")
+youstr = input("Enter Your Choice : ")
 youDict = {"s":1, "w":-1, "g":0 }
 you = youDict[youstr]
 
