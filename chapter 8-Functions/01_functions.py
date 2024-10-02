1a4a17e1b0b1de23bc70cab30a68205e136d0e56
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-02 22:15:12 +0530

    .

--- chapter 8-Functions/01_functions.py
+++ chapter 8-Functions/01_functions.py
@@ -2,8 +2,9 @@ def avg():
     a = int(input("Enter your first  number : "))
     b = int(input("Enter your Second number : "))
     c = int(input("Enter your Third  number : "))
+    d = int(input("Enter your fourth number : "))
 
-    average = (a+b+c)/3
+    average = (a+b+c+d)/3
     print(average)
 
 avg()
