306929319c2a94bf5219590d8f53d1000e804c24
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-04 21:56:03 +0530

    .

--- Project1 in Python/proj1.py
+++ Project1 in Python/proj1.py
@@ -1,28 +1,36 @@
 # # # We all have played snake, water gun game in our childhood.If you haven't google the rules and
 # # write the python code capable of playing by user 
 # write the code for it 
-computer = -1
-youstr = input("Enter Your Choice here : ")
-youDict = {"s":1, "w":-1, "g":0 }
-you = youDict[youstr]
+import random
 
-if(computer ==-1 and you ==1):
-    print("you win!!")
+# Snake, Water, Gun game
+def snake_water_gun():
+    choices = ["s", "w", "g"]  # Snake, Water, Gun choices
+    computer = random.choice(choices)  # Computer randomly selects
+    youstr = input("Enter Your Choice (s for Snake, w for Water, g for Gun): ").lower()
 
-elif(computer ==-1 and you ==0):
-    print("You Lost!")
-
-if(computer ==1 and you ==-1):
-    print("You Lose!")
-
-elif(computer ==1 and you == 0):
-    print("You Win!!") 
-
-if(computer ==0 and you ==-1):
-    print("You Win!")
-
-elif(computer ==0 and you ==1):
-    print("You Lose!!") 
+    # Dictionary to represent outcomes
+    youDict = {"s": 1, "w": -1, "g": 0}
     
-      
+    if youstr not in youDict:
+        print("Invalid input! Please enter 's', 'w', or 'g'.")
+        return
+    
+    # Map input to numbers
+    you = youDict[youstr]
+    computer_choice = youDict[computer]
 
+    # Determine the winner
+    if you == computer_choice:
+        result = "It's a draw!"
+    elif (you == 1 and computer_choice == -1) or (you == -1 and computer_choice == 0) or (you == 0 and computer_choice == 1):
+        result = "You win!"
+    else:
+        result = "You lose!"
+
+    # Output the results
+    print(f"Your choice: {youstr.upper()}, Computer's choice: {computer.upper()}")
+    print(result)
+
+# Run the game
+snake_water_gun()
